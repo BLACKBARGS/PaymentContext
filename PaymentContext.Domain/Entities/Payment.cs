2b1d54219cef35d0cf7817b158b6bca199bec939
Francys Rodrigues <119719373+BLACKBARGS@users.noreply.github.com>
2023-01-12 18:12:56 -0300

    Changes to be committed: 	modified:   PaymentContext.Domain/Entities/Payment.cs 	modified:   PaymentContext.Domain/Entities/Studant.cs 	modified:   PaymentContext.Domain/Entities/Subscription.cs

---
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,22 @@
+namespace PaymentContext.Domain.Entities
+{
+    public class Payment
+    {
+
+    }
+
+    public class BillPayment
+    {
+        
+    }
+
+    public class CreditCardPayment
+    {
+        
+    }
+
+    public class PaypalPayment
+    {
+        
+    }
+}
